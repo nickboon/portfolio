33a66977d751015f424a8e42c6b497db4e13f5bc
nickboon <boonnick@hotmail.com>
2017-03-17 23:40:21 +0000

    Separated offline and online json.

--- deploy/offline.js
+++ deploy/offline.js
@@ -1,7 +1,6 @@
 var fs = require('fs-extra');
 var addNormalizeCss = require('./normalizeCss');
 var addDependencies = require('./depMin');
-var deployMasonry = require('./masonry');
 
 function copy(src, out) {
 	fs.copy(src, out, function (err) {
